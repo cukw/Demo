69e3a399a7e7c7bfed942ef210c3d8f1dac738a7
cukw <kan142006@gmail.com>
2026-05-12 15:46:36 +0300

    testing avalonia for Demo

---
+++ Demo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DemoAvalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Demo/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DemoAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
